1cb69b4bb0f90bc80b55f9f8546f8d6018d60fce
factoryhands <jenniferlspriggs@gmail.com>
2023-09-03 03:35:50 +0000

    Adding the nav for when I get a project blog.

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ def index():
 @app.route('/generate', methods=['GET', 'POST'])
 def generate_text():
   response = openai.Completion.create(
-    engine="text-davinci-002",
+    engine="text-davinci-003",
     prompt=
-    "Write two paragraphs compelling a park's employee to bring back the hiker biker spots at Fay Bainbridge Park that were made non-reservable sites open to motorists. Do not start with a greeting. Do not end with a closing.",
+    "Write a kind but firm one paragraph statement requesting that the spots designated for hikers, bikers, and kayakers be restored at Fay Bainbridge Park. Do not include a greeting or begin the paragraph like a letter.",
     max_tokens=350)
   generated_text = response.choices[0].text.strip()
 
   return jsonify({'text': generated_text})
 
 
-app.run(host='0.0.0.0', port=81)
+app.run(host='0.0.0.0', port=8080)
